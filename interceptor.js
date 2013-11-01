804768d166bd37fe0c9facf0f36198bba912e78b
Jonas Follesø <jonas@follesoe.no>
2013-11-01 17:10:35 +0100

    Moved the replacement code into separate function.
    Can be refactored further to make it easy to add multiple map layers.

--- interceptor.js
+++ interceptor.js
@@ -1,8 +1,8 @@
-chrome.webRequest.onBeforeRequest.addListener(
-  function(details) {
-    if (details.type === 'image') {
+var interceptors = {
+  "statkart-topo2": {
+    replace: function(url) {
       var regex = /lyrs=([^&]+)(?:.*)&x=([^&]*)(?:.*)&y=([^&]*)(?:.*)&z=([^&]*)/;
-      var matches = regex.exec(details.url);
+      var matches = regex.exec(url);
       if (matches && matches.length > 1) {
         var layer = matches[1];
         var x = matches[2];
@@ -12,6 +12,18 @@ chrome.webRequest.onBeforeRequest.addListener(
         return {
           redirectUrl: url
         };
+      }
+      return null;
+    }
+  }
+}
+
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+    if (details.type === 'image') {
+      var res = interceptors["statkart-topo2"].replace(details.url);
+      if (res) { 
+        return res;
       }
     }
   },
