f7d6ceb845420c11ca78fcf9a89646215c6bb8a6
Jonas Follesø <jonas@follesoe.no>
2013-11-01 17:45:10 +0100

    Added multiple map layers form Statkart.
    Next is to add a UI to select which layer.

--- interceptor.js
+++ interceptor.js
@@ -1,19 +1,42 @@
+var statkart = function(layer, x, y, z) {
+  return "http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=" + layer + "&zoom=" + z + "&x=" + x + "&y=" + y;
+}
+
 var interceptors = {
   "statkart-topo2": {
-    replace: function(url) {
-      var regex = /lyrs=([^&]+)(?:.*)&x=([^&]*)(?:.*)&y=([^&]*)(?:.*)&z=([^&]*)/;
-      var matches = regex.exec(url);
-      if (matches && matches.length > 1) {
-        var layer = matches[1];
-        var x = matches[2];
-        var y = matches[3];
-        var z = matches[4];
-        var url = "http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=topo2&zoom=" + z + "&x=" + x + "&y=" + y;
-        return {
-          redirectUrl: url
-        };
-      }
-      return null;
+    name: "Statkart Topografisk",
+    replace: function(input) {
+      return statkart("topo2", input.x, input.y, input.z);
+    }
+  },
+  "sjo_hovedkart2": {
+    name: "Statkart Sjø Hovedkart",
+    replace: function(input) {
+      return statkart("sjo_hovedkart2", input.x, input.y, input.z);
+    }
+  },
+  "kartdata2": {
+    name: "Statkart Kartdata 2",
+    replace: function(input) {
+      return statkart("kartdata2", input.x, input.y, input.z);  
+    }
+  },
+  "topo2graatone": {
+    name: "Statkart Gråtone",
+    replace: function(input) {
+      return statkart("topo2graatone", input.x, input.y, input.z);    
+    }
+  },
+  "toporaster2": {
+    name: "Statkart Topografisk Raster",
+    replace: function(input) {
+      return statkart("toporaster2", input.x, input.y, input.z);      
+    }
+  },
+  "europa": {
+    name: "Statkart Europa",
+    replace: function(input) {
+      return statkart("europa", input.x, input.y, input.z);      
     }
   }
 }
@@ -21,9 +44,21 @@ var interceptors = {
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
     if (details.type === 'image') {
-      var res = interceptors["statkart-topo2"].replace(details.url);
-      if (res) { 
-        return res;
+      var regex = /lyrs=([^&]+)(?:.*)&x=([^&]*)(?:.*)&y=([^&]*)(?:.*)&z=([^&]*)/;
+      var matches = regex.exec(details.url);
+      if (matches && matches.length > 1) {
+        var input = {
+          x: matches[2],
+          y: matches[3],
+          z: matches[4]
+        };
+
+        var res = interceptors["toporaster2"].replace(input);
+        if (res) { 
+          return {
+            redirectUrl: res
+          };
+        }
       }
     }
   },
