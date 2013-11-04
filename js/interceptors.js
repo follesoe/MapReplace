e795aa77972ed373a6bb136a48c9b864942f6c68
Jonas Follesø <jonas@follesoe.no>
2013-11-04 09:02:47 +0100

    Added OpenStreetMap map layer.

--- js/interceptors.js
+++ js/interceptors.js
@@ -4,42 +4,53 @@ var interceptors = (function () {
   }
 
   return {
+    osm: {
+      name: "OpenStreetMap",
+      layers: {
+        "foo": {
+          name: "OpenStreetMap",
+          replace: function (input) {
+            return "http://tile.openstreetmap.org/" + input.z + "/" + input.x + "/" + input.y + ".png";
+          }
+        }
+      }
+    },
     statkart: {
       name: "Statkart",
       layers: {
         "statkart-topo2": {
           name: "Statkart Topografisk",
-          replace: function(input) {
+          replace: function (input) {
             return statkart("topo2", input);
           }
         },
         "sjo_hovedkart2": {
           name: "Statkart Sjø Hovedkart",
-          replace: function(input) {
+          replace: function (input) {
             return statkart("sjo_hovedkart2", input);
           }
         },
         "kartdata2": {
           name: "Statkart Kartdata 2",
-          replace: function(input) {
+          replace: function (input) {
             return statkart("kartdata2", input);
           }
         },
         "topo2graatone": {
           name: "Statkart Gråtone",
-          replace: function(input) {
+          replace: function (input) {
             return statkart("topo2graatone", input);
           }
         },
         "toporaster2": {
           name: "Statkart Topografisk Raster",
-          replace: function(input) {
+          replace: function (input) {
             return statkart("toporaster2", input);
           }
         },
         "europa": {
           name: "Statkart Europa",
-          replace: function(input) {
+          replace: function (input) {
             return statkart("europa", input);
           }
         }
