93125b65a2cd93ed36eb85d4453dbefde5162708
Jonas Follesø <jonas@follesoe.no>
2013-11-04 08:25:39 +0100

    Minor refactoring to interceptors.

--- js/interceptors.js
+++ js/interceptors.js
@@ -1,6 +1,6 @@
 var interceptors = (function () {
-  var statkart = function(layer, x, y, z) {
-    return "http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=" + layer + "&zoom=" + z + "&x=" + x + "&y=" + y;
+  var statkart = function(layer, input) {
+    return "http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=" + layer + "&zoom=" + input.z + "&x=" + input.x + "&y=" + input.y;
   }
 
   return {
@@ -10,37 +10,37 @@ var interceptors = (function () {
         "statkart-topo2": {
           name: "Statkart Topografisk",
           replace: function(input) {
-            return statkart("topo2", input.x, input.y, input.z);
+            return statkart("topo2", input);
           }
         },
         "sjo_hovedkart2": {
           name: "Statkart Sjø Hovedkart",
           replace: function(input) {
-            return statkart("sjo_hovedkart2", input.x, input.y, input.z);
+            return statkart("sjo_hovedkart2", input);
           }
         },
         "kartdata2": {
           name: "Statkart Kartdata 2",
           replace: function(input) {
-            return statkart("kartdata2", input.x, input.y, input.z);
+            return statkart("kartdata2", input);
           }
         },
         "topo2graatone": {
           name: "Statkart Gråtone",
           replace: function(input) {
-            return statkart("topo2graatone", input.x, input.y, input.z);
+            return statkart("topo2graatone", input);
           }
         },
         "toporaster2": {
           name: "Statkart Topografisk Raster",
           replace: function(input) {
-            return statkart("toporaster2", input.x, input.y, input.z);
+            return statkart("toporaster2", input);
           }
         },
         "europa": {
           name: "Statkart Europa",
           replace: function(input) {
-            return statkart("europa", input.x, input.y, input.z);
+            return statkart("europa", input);
           }
         }
       }
