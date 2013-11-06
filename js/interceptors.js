d751c113ac5683952c878f944ce65479e8794fb6
Mikael Brevik <mikaelbre@gmail.com>
2013-11-06 13:51:26 +0100

    Adds support for maps from Finn.no

--- js/interceptors.js
+++ js/interceptors.js
@@ -78,6 +78,29 @@ var interceptors = (function () {
           }
         }
       }
+    },
+    finn: {
+      name: "Finn.no",
+      layers: {
+        "finn-standard": {
+          name: "Standard",
+          replace: function (input) {
+            return "http://maptiles2.finncdn.no/tileService/1.0.1/normap/" + input.z + "/" + input.x + "/" + input.y + ".png"
+          }
+        },
+        "finn-flight": {
+          name: "Flyfoto",
+          replace: function (input) {
+            return "http://maptiles3.finncdn.no/tileService/1.0.1/norortho/" + input.z + "/" + input.x + "/" + input.y + ".png"
+          }
+        },
+        "finn-hybrid": {
+          name: "Hybrid",
+          replace: function (input) {
+            return "http://maptiles2.finncdn.no/tileService/1.0.1/norhybrid/" + input.z + "/" + input.x + "/" + input.y + ".png"
+          }
+        }
+      }
     }
   };
 })();
